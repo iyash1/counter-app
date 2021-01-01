bc1a800a55c83abc3756a3284e96138dc9da1bf6
Yash Eturi <yashwanthe@gmail.com>
2021-01-01 23:32:39 +0530

    ADDED: Bootstrap styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import {increment, decrement, reset} from './actions/action';
 import { connect } from 'react-redux';
+import { Button, Container, Row, Col } from 'reactstrap';
 
 class App extends Component {
   countAdd = () => {
@@ -18,15 +19,22 @@ class App extends Component {
 
   render(){
     return (
-      <div class='main'>
-        <Header />
-        <div class='count'>
-          <input type='button' id='add' value='+' onClick={this.countAdd}/>
-          <p> {this.props.count} </p>
-          <input type='button' id='sub' value='-' onClick={this.countSub}/>
-        </div>
-        <input type='button' id='reset' value='RESET' onClick={this.countReset}/>
-      </div>
+
+      <Container className="border border-primary rounded p-md-4 p-4 m-md-4 my-4">
+        <Row>
+          <Col md="12" className="text-center">
+            <Header />
+          </Col>
+        </Row>
+        <Row md="12">
+          <Col className="d-flex justify-content-around">
+            <Button id='add' color="success" onClick={this.countAdd} className="col-md-4 my-5" block>+</Button>{''}
+            <h1 className="col-md-4 my-5 text-center"> {this.props.count} </h1>
+            <Button id='sub' color="danger" onClick={this.countSub} className="col-md-4 my-5" block>-</Button>
+          </Col>
+        </Row>
+        <Button id='reset' color="warning" className="m-auto" onClick={this.countReset} block>RESET</Button>
+      </Container>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import {rootReducer} from './reducers/rootReducer';
 
